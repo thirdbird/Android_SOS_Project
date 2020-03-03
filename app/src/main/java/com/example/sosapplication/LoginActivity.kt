f64e599d29b3ae281ab721c2d65aeb52664a27f2
thirdbird <andreas@epaa.nu>
2020-03-03 18:16:03 +0100

    login and registration feature and firebase authentication implemented

---
+++ app/src/main/java/com/example/sosapplication/LoginActivity.kt
@@ -0,0 +1,84 @@
+package com.example.sosapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.btn_register
+import kotlinx.android.synthetic.main.activity_login.login_password
+import kotlinx.android.synthetic.main.activity_login.login_email
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_register.setOnClickListener{
+            startActivity(Intent(this,RegisterActivity::class.java))
+            finish()
+        }
+
+        btn_login.setOnClickListener{
+            performLogin()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun performLogin(){
+        if (!Patterns.EMAIL_ADDRESS.matcher(login_email.text.toString()).matches()){
+            login_email.error = getString(R.string.not_valid_email)
+            login_email.requestFocus()
+            return
+        }
+
+        if (login_password.text.toString().isEmpty()){
+            login_password.error = getString(R.string.no_password)
+            login_password.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(login_email.text.toString(), login_password.text.toString())
+            .addOnCompleteListener(this) {task ->
+                if (task.isSuccessful){
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(baseContext, getString(R.string.login_fail),
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?){
+        if (currentUser != null){
+            if (currentUser.isEmailVerified){
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(baseContext, getString(R.string.login_verify),
+                    Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(baseContext, getString(R.string.login_fail),
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sosapplication/RegisterActivity.kt
@@ -0,0 +1,64 @@
+package com.example.sosapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_register.setOnClickListener{
+            registerUser()
+        }
+    }
+
+    private fun registerUser(){
+        if (!Patterns.EMAIL_ADDRESS.matcher(register_email.text.toString()).matches()){
+            register_email.error = getString(R.string.not_valid_email)
+            register_email.requestFocus()
+            return
+        }
+
+        if (register_username.text.toString().isEmpty()){
+            register_username.error = getString(R.string.no_username)
+            register_username.requestFocus()
+            return
+        }
+
+        if (register_password.text.toString().isEmpty()){
+            register_password.error = getString(R.string.no_password)
+            register_password.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(register_username.text.toString(), register_password.text.toString())
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful){
+                    //sign in success, update UI with signed-in user's information.
+                    val user = auth.currentUser
+                    user?.sendEmailVerification()
+                        ?.addOnCompleteListener{task ->
+                            if (task.isSuccessful){
+                                startActivity(Intent(this,LoginActivity::class.java))
+                                finish()
+                            }
+                        }
+                } else {
+                    //if sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, getString(R.string.registration_fail),
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
